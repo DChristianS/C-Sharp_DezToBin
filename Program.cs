93e710b2816e348a68fa1768c07bb4414fd44ca1
DChristianS <75109660+DChristianS@users.noreply.github.com>
2021-04-01 11:02:07 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DezToBin
+{
+    class Program
+    {
+        public static String x(int n)
+        {
+            String s = "";
+            while (n > 0)
+            {
+                s = ((n % 2) == 0 ? "0" : "1") + s;
+                n = n/ 2;
+            }
+            return s;
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(x(0));
+            Console.WriteLine(x(5));
+            Console.WriteLine(x(42));
+        }
+    }
+}
